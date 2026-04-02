85b9e867315e758fe546a7d6ffda77c529169e64
Joseph Wonsil <joe.wonsil@gmail.com>
2026-04-02 13:05:09 -0700

    made the first version of our basic package

---
+++ R/math_functions.R
@@ -0,0 +1,27 @@
+#' Add two numbers
+#'
+#' @param a A number
+#' @param b A number
+#'
+#' @returns the sum of a and b 
+#' @export
+#'
+#' @examples
+#' add_numbers(2, 2)
+add_numbers <- function(a, b){
+  return(a + b)
+}
+
+#' Multiplies two numbers
+#'
+#' @param a A number
+#' @param b A number
+#'
+#' @returns the product of a and b 
+#' @export
+#'
+#' @examples
+#' multiply_numbers(2, 2)
+multiply_numbers <- function(a, b){
+  return(a * b)
+}
---
+++ tests/testthat/test-math_functions.R
@@ -0,0 +1,4 @@
+test_that("addition works correctly", {
+  expect_equal(add_numbers(2,2), 4)
+  expect_equal(add_numbers(-1,1), 0)
+})
